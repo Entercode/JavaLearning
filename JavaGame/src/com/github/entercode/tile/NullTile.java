07c7fd3a04a53b9ee7361f1ede88afba9db1aaf2
Entercode <quotenter@gmail.com>
2015-03-25 19:16:24 +0900

    Add new Tiles

---
+++ JavaGame/src/com/github/entercode/tile/NullTile.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.entercode.tile;
+
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author entercode
+ */
+public class NullTile extends Tile {
+
+	public NullTile(int id) {
+		super(id, "null");
+	}
+	
+	@Override
+	public void draw(Graphics2D g2d, int x, int y, int ox, int oy) {
+		
+	}
+
+	@Override
+	public void update() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
