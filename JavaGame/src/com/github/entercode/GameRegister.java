5dda294f76f7b0a518323f832a6b89e3829716d1
Entercode <quotenter@gmail.com>
2015-03-26 00:58:36 +0900

    Modify register method

--- JavaGame/src/com/github/entercode/GameRegister.java
+++ JavaGame/src/com/github/entercode/GameRegister.java
@@ -8,9 +8,14 @@ package com.github.entercode;
 import com.github.entercode.area.Area;
 import com.github.entercode.area.DebugArea;
 import com.github.entercode.entity.Entity;
+import com.github.entercode.tile.AirTile;
+import com.github.entercode.tile.DirtTile;
+import com.github.entercode.tile.IronFenceTile;
+import com.github.entercode.tile.NullTile;
+import com.github.entercode.tile.OreTile;
+import com.github.entercode.tile.StoneTile;
 import com.github.entercode.tile.Tile;
 import com.github.entercode.util.Log;
-import java.awt.Point;
 import java.util.ArrayList;
 
 /**
@@ -20,18 +25,37 @@ import java.util.ArrayList;
 public class GameRegister {
 	
 	// Entity Register
-	public static ArrayList<Entity> EntityRegister = new ArrayList<>();
-	public static ArrayList<Tile> TileRegister = new ArrayList<>();
+	public static ArrayList<Entity> EntityRegister = new ArrayList<>(1024);
+	public static ArrayList<Tile> TileRegister = new ArrayList<>(4096);
 	
 	// Render Register
-	public static Area currentArea = new DebugArea();
+	public static Area currentArea;
+	
+	public static void init() {
+		
+		Log.info("array...", "init");
+		
+		for(int i = 0; i < 1024; i++) {
+			EntityRegister.add(null);
+		}
+		
+		for(int i = 0; i < 4096; i++) {
+			TileRegister.add(null);
+		}
+		
+		Log.info(TileRegister.size());
+	}
 
 	
 	public static void load() {
 		
-		loadMap();
-		loadEntity();
+		init();
 		
+		loadArea();
+		loadEntity();
+		loadTile();
+		
+		currentArea = new DebugArea();
 	}
 	
 	public static void loadEntity() {
@@ -39,16 +63,30 @@ public class GameRegister {
 		
 	}
 	
-	public static void loadMap() {
-		Log.info("load Map");
+	public static void loadTile() {
+		
+		register(0,		new NullTile		(0, "Null"));
+		register(7,	new AirTile			(7, "Air"));
+		register(8,	new DirtTile		(8, "Dirt"));
+		register(9,		new IronFenceTile	(9, "IronFence"));
+		register(11,		new OreTile			(11, "Ore"));
+		register(14,	new StoneTile		(14, "Stone"));
+		register(13,		new StoneTile			(13, "Stone"));
+		
+		Log.info("loaded Tile");
+		System.out.printf("%d", TileRegister.size());
 		
 	}
-
-	public static void register(Entity par0) {
-		EntityRegister.add(par0);
+	
+	public static void loadArea() {
+		Log.info("load Area");
 	}
 
-	public static void register(Tile par0) {
-		TileRegister.add(par0);
+	public static void register(int id, Entity par0) {
+		EntityRegister.set(id, par0);
+	}
+
+	public static void register(int id, Tile par0) {
+		TileRegister.set(id, par0);
 	}
 }
