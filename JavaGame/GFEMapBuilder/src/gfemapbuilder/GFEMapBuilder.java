3b6242a23a30b1b24ed84778e25d59c470cd5a1f
Entercode <quotenter@gmail.com>
2015-03-25 19:45:29 +0900

    Add GameFrameEngineMapBuilder project

---
+++ JavaGame/GFEMapBuilder/src/gfemapbuilder/GFEMapBuilder.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfemapbuilder;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author entercode
+ */
+public class GFEMapBuilder {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		
+		JFrame app = new JFrame("GFE MapBuilder");
+		app.setSize(800, 600);
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.setVisible(true);
+		
+		
+	}
+	
+}
